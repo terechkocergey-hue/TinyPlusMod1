72d4fdbff5b10e76e987631da13d5323eb823a98
terechkocergey-hue <terechkocergey@gmail.com>
2026-04-05 19:47:09 +0300

    Update TinyPlusMod.java

--- src/main/java/com/tinyplus/TinyPlusMod.java
+++ src/main/java/com/tinyplus/TinyPlusMod.java
@@ -4,28 +4,39 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TinyPlusMod implements ModInitializer {
-    public static final String MOD_ID = "tinyplus";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
     @Override
     public void onInitialize() {
-        LOGGER.info("TinyPlus Mod инициализирован!");
-
-        // Регистрация команды /smoll
+        System.out.println("TinyPlus Mod загружен!");
+        
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(CommandManager.literal("smoll")
-                .then(CommandManager.argument("player", net.minecraft.command.argument.EntityArgumentType.player())
-                    .executes(context -> {
-                        var player = net.minecraft.command.argument.EntityArgumentType.getPlayer(context, "player");
-                        player.sendMessage(Text.literal("§a✨ Ты уменьшился! ✨"), false);
-                        context.getSource().sendMessage(Text.literal("§aТы уменьшил " + player.getName().getString()));
-                        return 1;
-                    })
-                )
+                .executes(context -> {
+                    context.getSource().sendMessage(Text.literal("§a✨ Ты уменьшился! ✨"));
+                    return 1;
+                })
+            );
+            
+            dispatcher.register(CommandManager.literal("unsmoll")
+                .executes(context -> {
+                    context.getSource().sendMessage(Text.literal("§c📏 Ты вернул нормальный размер!"));
+                    return 1;
+                })
+            );
+            
+            dispatcher.register(CommandManager.literal("sit")
+                .executes(context -> {
+                    context.getSource().sendMessage(Text.literal("§e💺 Ты сел!"));
+                    return 1;
+                })
+            );
+            
+            dispatcher.register(CommandManager.literal("lie")
+                .executes(context -> {
+                    context.getSource().sendMessage(Text.literal("§7🛌 Ты лёг!"));
+                    return 1;
+                })
             );
         });
     }
