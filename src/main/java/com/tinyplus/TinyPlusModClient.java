bfbca749e88a1c3b8ac0a2b8bb1d16e9b3f90cda
terechkocergey-hue <terechkocergey@gmail.com>
2026-04-05 20:08:47 +0300

    Update TinyPlusModClient.java

--- src/main/java/com/tinyplus/TinyPlusModClient.java
+++ src/main/java/com/tinyplus/TinyPlusModClient.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class TinyPlusModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        System.out.println("TinyPlus Mod клиент инициализирован!");
+        System.out.println("TinyPlus Mod Client загружен!");
     }
 }
